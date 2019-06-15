75ce71f54bafb12f9a6836784b120824019e2a99
Alexandru Burghelea <burghelea.alex@gmail.com>
2019-06-15 15:43:21 +0300

    code splitting using async

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Page1 from './components/Page1'
+import Page1 from './components/Page1';
 import './App.css';
-
+import AsyncComponent from'./components/AsyncComponent';
 
 class App extends React.Component{
   constructor(){
       super();
       this.state={
           route:'page1',
-          component:''
+          component:null
       }
   }
   onRouteChange = (route) => {
       //No code splitting:
-      //this.setState({route:route});
+      this.setState({route:route});
       //With code splitting:
+      /*
       switch (route) {
           case 'page1':
               this.setState({route:route}); break;
@@ -38,16 +39,31 @@ class App extends React.Component{
               this.setState({route:route});break;
 
 
-      }
+      }*/
   }
 
   render(){
+
+      switch (this.state.route ) {
+          case 'page1':
+            return <Page1 onRouteChange={this.onRouteChange}/>
+          case 'page2': {
+              const AsyncPage2= AsyncComponent(()=>import('./components/Page2'));
+              return <AsyncPage2 onRouteChange ={this.onRouteChange}/>
+          }
+          case 'page3':{
+              const AsyncPage3=AsyncComponent(()=>import('./components/Page3'));
+              return <AsyncPage3 onRouteChange ={this.onRouteChange}/>
+          }
+      }
+
+      /*
       if(this.state.route ==='page1'){
           return <Page1 onRouteChange={this.onRouteChange}/>
       }else {
           return <this.state.component onRouteChange={this.onRouteChange}/>
       }
-
+       */
   }
 }
 
